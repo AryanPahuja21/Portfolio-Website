03ed1a6b1b52fb6cf2836c493c8584d22b1b4486
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-03-12 23:08:46 +0530

    contact toaster added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] absolute top-[-6rem] -z-10 left-[-15rem] h-[31.25rem] w-[30rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <Header/>
         {children}
+        <Toaster position="top-right"/>
         </body>
     </html>
   );
