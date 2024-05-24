72681144059306408ce9a3872f8c4609ea680f2c
AryanPahuja21 <aryanpahuja1421@gmail.com>
2024-05-25 00:54:50 +0530

    hero flip animation created

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { FlipWords } from "../ui/flip-words";
 
 const HeroSection = () => {
   return (
-    <div>
-      <h1>HI. I am Aryan Pahuja</h1>
-    </div>
+    <>
+      <main>
+        <div className="bg-[#101010] h-screen flex flex-col gap-5 items-center justify-center">
+          <h1 className="text-white text-5xl font-bold">Hey, I'm</h1>
+          <div className="text-5xl font-bold">
+            <FlipWords
+              words={["", "a"]}
+              duration={3000}
+              className="text-red-600"
+            />
+            <FlipWords
+              words={["Aryan", "Web"]}
+              duration={3000}
+              className="text-red-600"
+            />
+            <FlipWords
+              words={["Pahuja", "Developer"]}
+              duration={3000}
+              className="text-red-600"
+            />
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
